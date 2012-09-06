0f9e2cd88616fcaae1bd0a0e7a03ecb415695684
Will Pimblett <will@fullaf.com>
2012-09-06 16:14:57 +0100

    old commit from TWI

---
+++ tickets/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from tickets.models import *
+
+admin.site.register(Ticket_Type)
+admin.site.register(Show)
+admin.site.register(Occurrence)
+admin.site.register(Ticket)
--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Ticket_Type(models.Model):
+	name=models.CharField(max_length=30)
+	price=models.DecimalField(max_digits=4, decimal_places=2)
+	def __unicode__(self):
+		return self.name+" L"+str(self.price)
+
+class Show(models.Model):
+	name=models.CharField(max_length=30)
+	location=models.CharField(max_length=30, default='Theatre')
+	def __unicode__(self):
+		return self.name;
+
+class Occurrence(models.Model):
+	show=models.ForeignKey(Show)
+	date=models.DateField()
+	time=models.TimeField()
+	maximum_sell=models.PositiveIntegerField()
+	hours_til_close=models.IntegerField(default=3)
+	tickets_available=models.ManyToManyField(Ticket_Type)
+
+	def __unicode__(self):
+		return self.show.name+" on "+str(self.date)+" at "+str(self.time)
+
+class Ticket(models.Model):	
+	occurrence=models.ForeignKey(Occurrence)
+	stamp=models.DateTimeField(auto_now=True)
+	person_name=models.CharField(max_length=40)
+	email_address=models.EmailField(max_length=40)
+	type=models.ForeignKey(Ticket_Type)
