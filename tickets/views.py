d392a93321e504ce7fc75c5d64b8a3bc63cba3ef
Will Pimblett <will@fullaf.com>
2013-06-26 11:52:46 +0100

    fix broken links on list page (pass settings to template)

--- tickets/views.py
+++ tickets/views.py
@@ -82,6 +82,13 @@ class ListShows(OrderedListView):
     context_object_name='shows'
     order_by='start_date'
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ListShows, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['settings'] = settings
+        return context
+
 def sidebar(request):
     categories=Category.objects.all().exclude(sort=0).order_by('sort')
     today=datetime.date.today()
