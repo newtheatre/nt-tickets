f6419c082e30f514e373fdfbcb9ddcf69a6bec6c
Will Pimblett <will@fullaf.com>
2013-07-07 22:52:27 +0100

    include email settings in settings files

--- configuration/staging.py
+++ configuration/staging.py
@@ -14,3 +14,10 @@ DATABASES = {
 
 SITE_URL="http://localhost:8000"
 MEDIA_URL = "http://localhost:8000/media/"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.example.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'username'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_USE_TLS = True
