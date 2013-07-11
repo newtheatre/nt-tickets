917a034cc5148f8000323419df4405ab65f69d8a
Will Pimblett <will@fullaf.com>
2013-07-11 01:09:57 +0100

    restrict what's avaliable via show api

--- tickets/api.py
+++ tickets/api.py
@@ -10,3 +10,6 @@ class ShowResource(ModelResource):
         today=datetime.date.today()
         queryset = Show.objects.filter(end_date__gte=today)
         resource_name = 'show'
+        fields=['name','slug','location','description','poster','poster_wall',
+        'poster_page','poster_tiny', 'start_date','end_date', 'category']
+        allowed_methods = ['get']
