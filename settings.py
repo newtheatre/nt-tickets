02117fedce1e9134fca03ad0fa5bea01e91769d0
Harry Bridge <harrybridge01@gmail.com>
2015-11-11 19:14:07 +0000

    Fork of old repo, with some updates

--- settings.py
+++ settings.py
@@ -126,11 +126,12 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'south',
-    
+    #'south',
+
     'bootstrap_toolkit',
     'tickets',
 )
+
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
