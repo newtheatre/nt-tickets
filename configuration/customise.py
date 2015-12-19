2d8b86971e54e75701dd549aa2dd59f2fa100c7b
Harry Bridge <harrybridge01@gmail.com>
2015-12-19 22:59:29 +0000

    Improved readability

--- configuration/customise.py
+++ configuration/customise.py
@@ -1,11 +1,11 @@
 import datetime
 
-ORG_NAME="New Theatre"
-ORG_CONTACT="boxoffice@newtheatre.org.uk"
+ORG_NAME = "New Theatre"
+ORG_CONTACT = "boxoffice@newtheatre.org.uk"
 
-DEFAULT_LOCATION="New Theatre"
-DEFAULT_TIME=datetime.time(19,30)
-DEFAULT_MAX_SELL=80
-DEFAULT_HOURS_TIL_CLOSE=2
+DEFAULT_LOCATION = "New Theatre"
+DEFAULT_TIME = datetime.time(19, 30)
+DEFAULT_MAX_SELL = 80
+DEFAULT_HOURS_TIL_CLOSE = 2
 
-SIDEBAR_FILTER_PERIOD=datetime.timedelta(weeks=3)
+SIDEBAR_FILTER_PERIOD = datetime.timedelta(weeks=3)
