c001db859979f462672abf63e64e2ecc7d3bc1c9
Harry Bridge <harrybridge01@gmail.com>
2016-01-08 19:35:10 +0000

    Settings files for fabric

--- settings.py
+++ settings.py
@@ -15,17 +15,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',     # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite3',                       # Or path to database file if using sqlite3.
-        'USER': '',                                 # Not used with sqlite3.
-        'PASSWORD': '',                             # Not used with sqlite3.
-        'HOST': '',                                 # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                                 # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 SITE_URL = "http://localhost:8000"
 
 # Local time zone for this installation. Choices can be found here:
