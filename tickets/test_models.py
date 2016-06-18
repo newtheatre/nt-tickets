7f2ce1ff791664cacf135364d92b3d2a054f4be3
Harry Bridge <harrybridge01@gmail.com>
2016-06-18 23:53:56 +0100

    Move all model tests to be class based

--- tickets/test_models.py
+++ tickets/test_models.py
@@ -21,40 +21,37 @@ class StaticPageTest(TestCase):
 
 class BookTest(TestCase):
 
-    def setUp(self):
-        cat = Category.objects.create(name='Test Category', slug='test', sort=1)
-        start_date = date.today() + timedelta(days=2)
-        end_date = date.today() + timedelta(days=5)
-        Show.objects.create(
+    @classmethod
+    def setUpTestData(cls):
+        cls.cat = Category.objects.create(name='Test Category', slug='test', sort=1)
+        cls.start_date = date.today() + timedelta(days=2)
+        cls.end_date = date.today() + timedelta(days=5)
+        cls.show = {
+        1: Show.objects.create(
             name='Test Show',
             location='Somewhere',
             description='Some Info',
-            long_description='Some more info',
-            poster=File(open('test/test_poster.jpg')),
-            start_date=start_date,
-            end_date=end_date,
-            category=cat
-        )
+            start_date=cls.start_date,
+            end_date=cls.end_date,
+            category=cls.cat
+            )
+        }
 
     def test_category_name(self):
-        cat = Category.objects.get(pk=1).__str__()
-        self.assertEqual(cat, 'Test Category')
+        self.assertEqual(self.cat.__str__(), 'Test Category')
 
     def test_show_exists(self):
-        show = Show.objects.get(pk=1)
-        self.assertEqual(show.name, 'Test Show')
+        self.assertEqual(self.show[1].name, 'Test Show')
 
     def test_book_form(self):
         response = self.client.get('/book/1/')
         self.assertEqual(response.status_code, 200)
 
     def test_show_sold_out_false(self):
-        show = Show.objects.get(pk=1)
-        self.assertEqual(show.show_sold_out(), False)
+        self.assertEqual(self.show[1].show_sold_out(), False)
 
     def test_has_occurrences_false(self):
-        show = Show.objects.get(pk=1)
-        self.assertEqual(show.has_occurrences(), False)
+        self.assertEqual(self.show[1].has_occurrences(), False)
 
 
 class ShowTest(TestCase):
@@ -70,7 +67,8 @@ class ShowTest(TestCase):
         # Create some good shows
         cls.show = {
             1: Show.objects.create(name='S1', category=cls.cat, description='show current', long_description=cls.l_desc, start_date=cls.today, end_date=cls.today + timedelta(days=6)),
-            2: Show.objects.create(name='S2', category=cls.cat, description='show past', long_description=cls.l_desc, start_date=cls.today - timedelta(days=6), end_date=cls.today),
+            2: Show.objects.create(name='S2', category=cls.cat, description='show past', long_description=cls.l_desc, start_date=cls.today - timedelta(days=6), end_date=cls.today - timedelta(days=1)),
+            3: Show.objects.create(name='S3', category=cls.cat, description='show closed', long_description=cls.l_desc, start_date=cls.today, end_date=cls.today + timedelta(days=4)),
         }
 
         # Create an occurrence
@@ -78,61 +76,43 @@ class ShowTest(TestCase):
 
         cls.occ = {
             1: Occurrence.objects.create(show=cls.show[1], date=occ_day.date(), time=occ_day.time(), maximum_sell=2, hours_til_close=0),
+            2: Occurrence.objects.create(show=cls.show[2], date=occ_day.date(), time=occ_day.time(), maximum_sell=2, hours_til_close=0),
+            3: Occurrence.objects.create(show=cls.show[3], date=datetime.now().date(), time=datetime.now().time(), maximum_sell=2, hours_til_close=2),
         }
 
-        cls.ticket = Ticket.objects.create(
-            occurrence=cls.occ[1],
-            person_name='testman',
-            email_address='test@test.com',
-            quantity=1,
-        )
+        cls.ticket = {
+            1: Ticket.objects.create(occurrence=cls.occ[1], person_name='testman', email_address='test@test.com', quantity=1),
+            2: Ticket.objects.create(occurrence=cls.occ[2], person_name='testman2', email_address='test@test.com', quantity=2),
+            3: Ticket.objects.create(occurrence=cls.occ[3], person_name='testman3', email_address='test@test.com', quantity=1),
+        }
+        
 
         cls.sale = Sale.objects.create(
             occurrence=cls.occ[1], ticket='None', price=1, number=2)
 
     def test_is_current_false(self):
-        show = Show.objects.get(name='S1')
-        show.end_date = date.today() + timedelta(days=-5)
-        self.assertEqual(show.is_current(), False)
+        self.assertEqual(self.show[2].is_current(), False)
 
     def test_sold_out_true(self):
-        show = Show.objects.get(name='S1')
-        occ = Occurrence.objects.get(show=show)
-        Ticket.objects.create(
-            occurrence=Occurrence.objects.get(pk=1),
-            stamp=datetime.now(),
-            person_name='testman2',
-            email_address='test@test.com',
-            quantity=1,
-            cancelled=False,
-            unique_code=rand_16(),
-        )
-
-        self.assertTrue(show.show_sold_out())
-        self.assertTrue(occ.sold_out())
+        self.assertTrue(self.show[2].show_sold_out())
+        self.assertTrue(self.occ[2].sold_out())
 
     def test_sold_out_false(self):
-        show = Show.objects.get(name='S1')
-        occ = Occurrence.objects.get(show=show)
-        self.assertFalse(occ.sold_out())
-        self.assertFalse(show.show_sold_out())
+        self.assertFalse(self.show[1].show_sold_out())
+        self.assertFalse(self.occ[1].sold_out())
 
     def test_has_occurrences_true(self):
-        show = Show.objects.get(name='S1')
-        self.assertTrue(show.has_occurrences())
+        self.assertTrue(self.show[1].has_occurrences())
 
     def test_show_name(self):
-        show = Show.objects.get(name='S1')
-        self.assertEqual(show.__str__(), show.name)
+        self.assertEqual(self.show[1].__str__(), self.show[1].name)
 
     def test_markdown(self):
-        show = Show.objects.get(name='S1')
         ld_md = '<p>A longer show somewhere</p>\n'
-        self.assertEqual(show.long_markdown(), ld_md)
+        self.assertEqual(self.show[1].long_markdown(), ld_md)
 
     def test_datetime_formatted(self):
-        show = Show.objects.get(name='S1')
-        occ = Occurrence.objects.get(show=show)
+        occ = self.occ[1]
         day_format = occ.date.strftime('%A')
         time_format = occ.time.strftime('%-I:%M %p').lower()
         datetime_format = occ.date.strftime('%A %d %B ') + \
@@ -143,17 +123,14 @@ class ShowTest(TestCase):
         self.assertEqual(occ.datetime_formatted(), datetime_format)
 
     def test_occurrence_str(self):
-        show = Show.objects.get(name='S1')
-        occ = Occurrence.objects.get(show=show)
+        occ = self.occ[1]
         occ_str = occ.show.name + \
             " on " + str(occ.day_formatted()) + \
             " at " + str(occ.time_formatted())
         self.assertEqual(occ.__str__(), occ_str)
 
     def test_ticket_str(self):
-        show = Show.objects.get(name='S1')
-        occ = Occurrence.objects.get(show=show)
-        tick = Ticket.objects.get(occurrence=occ)
+        tick = self.ticket[1]
         tick_str = tick.occurrence.show.name + \
             " on " + str(tick.occurrence.date) + \
             " at " + str(tick.occurrence.time) + \
@@ -162,25 +139,18 @@ class ShowTest(TestCase):
         self.assertEqual(tick.__str__(), tick_str)
 
     def test_get_available(self):
-        show = Show.objects.get(name='S1')
-        occ = Occurrence.objects.get(show=show)
+        occ = self.occ[1]
         datetime_format = occ.date.strftime('%A %d %B ') + \
             occ.time.strftime('%-I:%M %p').lower()
 
-        r1 = Occurrence.objects.get_available(show)
+        r1 = Occurrence.objects.get_available(self.show[1])
 
         self.assertEqual(occ.sold_out(), False)    # Sanity Check
         self.assertEqual(r1, [(1, datetime_format)])
 
     def test_get_available_sold_out(self):
-        show = self.show[1]
-        occ = self.occ[1]
-        Ticket.objects.create(
-            occurrence=occ,
-            person_name='testman2',
-            email_address='test@test.com',
-            quantity=2,
-        )
+        show = self.show[2]
+        occ = self.occ[2]
 
         r = Occurrence.objects.get_available(show)
 
@@ -190,85 +160,15 @@ class ShowTest(TestCase):
         self.assertEqual(r, [])
 
     def test_show_sales(self):
-        show = Show.objects.get(pk=1)
-        occ = Occurrence.objects.get(pk=1)
-
-        self.assertEqual(show.show_sales(), 1)
-
-
-class ShowClosed(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        cat = Category.objects.create(name='Test Category', slug='test', sort=1)
-        start_date = date.today() - timedelta(days=2)
-        end_date = date.today() + timedelta(days=5)
-        cls.show = Show.objects.create(
-            name='Test Show',
-            start_date=start_date,
-            end_date=end_date,
-            category=cat
-        )
-
-        cls.occ = Occurrence.objects.create(
-            show=cls.show,
-            date=date.today(),
-            time=datetime.now(),
-            maximum_sell=2,
-            hours_til_close=2,
-            unique_code=rand_16(),
-        )
-
-        cls.ticket = Ticket.objects.create(
-            occurrence=cls.occ,
-            person_name='testman',
-            email_address='test@test.com',
-            quantity=1,
-        )
+        self.assertEqual(self.show[1].get_sale_data()['show_sales'], 1)
 
     def test_get_available_show_closed(self):
-        show = self.show
-        r = Occurrence.objects.get_available(show)
+        r = Occurrence.objects.get_available(self.show[3])
         self.assertEqual(r, [])
 
     def test_get_available_show_show_closed(self):
-        show = self.show
-        occ = self.occ
-
-        r = Occurrence.objects.get_available_show(show)
-
+        r = Occurrence.objects.get_available_show(self.show[3])
         self.assertNotEqual(r, [])
-
-
-class ShowReallyClosed(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        cat = Category.objects.create(name='Test Category', slug='test', sort=1)
-        start_date = date.today() - timedelta(days=2)
-        end_date = date.today() + timedelta(days=5)
-        cls.show = Show.objects.create(
-            name='S1',
-            start_date=start_date,
-            end_date=end_date,
-            category=cat
-        )
-
-        occ_day = datetime.now() - timedelta(hours=7)
-
-        cls.occ = Occurrence.objects.create(
-            show=cls.show,
-            date=occ_day.date(),
-            time=occ_day.time(),
-            maximum_sell=2,
-            hours_til_close=2,
-            unique_code=rand_16(),
-        )
-
-    def test_get_available_show_closed(self):
-        show = self.show
-        r = Occurrence.objects.get_available(show)
-        self.assertEqual(r, [])
 
 
 class SaleTest(TestCase):
@@ -314,36 +214,32 @@ class SaleTest(TestCase):
                                        )
 
     def test_show_sales(self):
-        show = self.show
-        profit = show.show_sales()
+        profit = self.show.get_sale_data()['show_sales']
         self.assertEqual(profit, 53)
 
     def test_total_tickets_sold_show(self):
-        show = self.show
-        tickets = show.total_tickets_sold_show()
+        tickets = self.show.get_sale_data()['total_sold']
         self.assertEqual(tickets, 18)
 
     def test_total_tickets_reserved(self):
-        show = self.show
-        reserved = show.total_tickets_reserved()
+        reserved = self.show.get_sale_data()['total_reserved']
         self.assertEqual(reserved, 1)
 
     def test_total_possible(self):
-        show = self.show
-        total = show.total_possible()
+        total = self.show.get_sale_data()['total_possible']
         self.assertEqual(total, 80)
 
     def test_tallys(self):
         occ = self.occ
-        concession = occ.concession_tally()
-        member = occ.member_tally()
-        public = occ.public_tally()
-        season = occ.season_tally()
-        season_sale = occ.season_sale_tally()
-        fellow = occ.fellow_tally()
-        fringe = occ.fringe_tally()
-        mat_f = occ.matinee_freshers_tally()
-        mat_f_nnt = occ.matinee_freshers_nnt_tally()
+        concession = occ.get_tally('concession')
+        member = occ.get_tally('member')
+        public = occ.get_tally('public')
+        season = occ.get_tally('season')
+        season_sale = occ.get_tally('season_sale')
+        fellow = occ.get_tally('fellow')
+        fringe = occ.get_tally('fringe')
+        mat_f = occ.get_tally('matinee_freshers')
+        mat_f_nnt = occ.get_tally('matinee_freshers_nnt')
 
         tot = concession + member + public + season + \
             season_sale + fellow + fringe + mat_f + mat_f_nnt
