8a06a7a50659b9f4f22087708d3e486093fac529
Harry Bridge <harry@harrybridge.co.uk>
2016-08-27 16:44:31 +0100

    Added apps.py

--- pricing/apps.py
+++ pricing/apps.py
@@ -1,5 +1,9 @@
+from __future__ import unicode_literals
+
 from django.apps import AppConfig
 
 
 class PricingConfig(AppConfig):
     name = 'pricing'
+    verbose_name = 'Pricing'
+    verbose_name_plural = 'Pricing'
