2393872c6a3cd64c0675424e32549d692d61a131
David Taylor <david@taylorhq.com>
2019-02-26 17:00:43 +0000

    Increase API page size

--- NT_Tickets/settings.py
+++ NT_Tickets/settings.py
@@ -137,7 +137,7 @@ WSGI_APPLICATION = 'NT_Tickets.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 100
 }
 
 # Database
