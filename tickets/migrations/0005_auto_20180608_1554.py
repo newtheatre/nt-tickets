bb1194d7bf329589d5cba59f8e55c632343e5187
Sam Osborne <sam.tosborne@googlemail.com>
2019-05-11 00:56:17 +0100

    Fix migration fork
    Master has had migrations since last updates to this branch

--- tickets/migrations/0005_auto_20180608_1554.py
+++ tickets/migrations/0005_auto_20180608_1554.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tickets', '0004_auto_20161024_1658'),
+        ('tickets', '0005_auto_20171031_2322'),
     ]
 
     operations = [
--- tickets/migrations/0006_auto_20190511_0020.py
+++ tickets/migrations/0006_auto_20190511_0020.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tickets', '0005_auto_20171031_2322'),
+        ('tickets', '0005_auto_20180608_1554'),
     ]
 
     operations = [
